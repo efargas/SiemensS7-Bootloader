6c2f6cfd5b3947b4f5b779b747718b8ec07f5144
Erik Fargas <efargas@users.noreply.github.com>
2025-06-18 12:53:39 +0200

    Update client.sh

--- client.sh
+++ client.sh
@@ -1,2 +1,12 @@
+A=--port
+B=1238
+C=--method
+D=fx3u
+E=--powersupply-fx3u-ip
+F=192.168.1.18
+G=--powersupply-fx3u-port
+H=502 
+I=--powersupply-fx3u-output
+J=Y0
 echo Remaining arguments: "$@"
-python3 client.py --method fx3u --ip 192.168.1.18 --port 502 --output 0 "$@"
+python3 client.py $A $B $C $D $E $F $G $H $I $J "$@"
