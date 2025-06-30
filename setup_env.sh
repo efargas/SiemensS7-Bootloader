0006ae27d152c72c09e2ac5c4ff657b5a902ab8a
efargas <efargaspro@gmail.com>
2025-06-30 06:41:08 -0700

    Update setup_env.sh script

--- setup_env.sh
+++ setup_env.sh
@@ -1,23 +1,61 @@
 #!/bin/bash
+set -e
 
-# Install dependencies
+echo "Updating package lists..."
 sudo apt-get update
-sudo apt-get install -y python2 python3 python3-venv python-is-python3
 
-# Create Python 3 virtual environment
-python3 -m venv venv3
+echo "Installing system packages..."
+sudo apt-get install -y \
+    build-essential \
+    gcc-arm-none-eabi \
+    binutils-arm-none-eabi \
+    clang=1:6.0-41~exp5~ubuntu1 \
+    make \
+    socat \
+    python2.7 \
+    python-pip \
+    git \
+    libffi-dev \
+    libssl-dev \
+    python2.7-dev \
+    virtualenv \
+    binutils \
+    gcc \
+    binutils-arm-linux-gnueabi \
+    gcc-arm-linux-gnueabi \
+    python2 \
+    python3 \
+    python3-venv \
+    python-is-python3
 
-# Activate Python 3 virtual environment and install dependencies
-. venv3/bin/activate
-pip install -r requirements-py3.txt
+echo "Creating Python 3 virtual environment..."
+python3 -m venv ~/venv3
+
+echo "Activating Python 3 virtual environment and install dependencies..."
+. ~/venv3/bin/activate
+
+echo "Upgrading pip3 in virtualenv..."
+pip3 install --upgrade pip
+
+echo "Installinq requirements for Python 3 on virtualenv..."
+pip3 install -r requirements-py3.txt
+
+echo "Deactivating Python 3 on virtualenv..."
 deactivate
 
-# Create Python 2 virtual environment
-python2 -m virtualenv venv2
+echo "Creating Python 2 virtual environment..."
+python2 -m virtualenv ~/venv2
 
-# Activate Python 2 virtual environment and install dependencies
-. venv2/bin/activate
-pip install -r requirements-py2.txt
+echo "Activating Python 2 virtual environment and install dependencies..."
+. ~/venv2/bin/activate
+
+echo "Upgrading pip2 in virtualenv..."
+pip2 install --upgrade pip
+
+echo "Installinq requirements for Python 2 on virtualenv..."
+pip2 install -r requirements-py2.txt
+
+echo "Deactivating Python 2 on virtualenv..."
 deactivate
 
 # Set up update-alternatives
@@ -26,5 +64,5 @@ sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 2
 
 echo "Setup complete."
 echo "To switch between Python versions, use: sudo update-alternatives --config python"
-echo "To activate the Python 3 virtual environment, run: . venv3/bin/activate"
-echo "To activate the Python 2 virtual environment, run: . venv2/bin/activate"
+echo "To activate the Python 3 virtual environment, run: . ~/venv3/bin/activate"
+echo "To activate the Python 2 virtual environment, run: . ~/venv2/bin/activate"
