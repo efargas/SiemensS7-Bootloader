c795af67f3a06576085f1a61cb05be054ce5c8c2
Erik Fargas <efargas@users.noreply.github.com>
2025-12-11 12:57:16 +0100

    Update client.py
    Co-authored-by: qodo-code-review[bot] <151058649+qodo-code-review[bot]@users.noreply.github.com>

--- client.py
+++ client.py
@@ -249,7 +249,8 @@ class SiemensS7Client:
         return True
 
     def handle_connection(self, args):
-        log.success("[+] Got special access greeting: {} [{}]".format(self.r.recv(5).encode('hex'), self.r.recv(5).encode('hex')))
+        greeting = self.r.recv(5)
+        log.success("[+] Got special access greeting: {} [{}]".format(greeting.encode('hex'), greeting.encode('hex')))
 
         version = self.get_version()
         bootloaderversion = version[2:3] + ".".join([str(ord(c)) for c in version[3:-2]])
